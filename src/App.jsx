a5d1840f201cb5cdd25ef47702ecc9970484e8c4
vishal <vishalkumar9334467@gmail.com>
2025-06-05 01:07:15 +0530

    feat: add password length slider and improve styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,85 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { LC, NC, SC, UC } from './Data/PassChar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [uppercase, setUppercase] = useState(false)
+  const [lowercase, setLowercase] = useState(false)
+  const [number, setNumber] = useState(false)
+  const [symbol, setSymbol] = useState(false)
+  const [passlen, SetPasslen]= useState(10)
+  const [fpass , setPass]= useState('')
+
+  
+
+  let createPasword=()=>{
+    let finalPass=""
+    let charSet=""
+    if(uppercase || lowercase || number || symbol ){
+       if (uppercase) charSet+=UC;
+       if(lowercase)  charSet+=LC;
+       if(number)     charSet+=NC;
+       if(symbol)     charSet+=SC;
+
+       for ( let i=0; i<passlen; i++){
+          finalPass += charSet.charAt(Math.floor(Math.random()*charSet.length)) 
+       }
+      
+          setPass(finalPass)
+
+    }
+    else{
+      alert("please select atleast one cheakbox")
+    }
+  }
+
+  let copyPass=()=>{
+    navigator.clipboard.writeText(fpass)
+  }
+
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='passbox'>
+        <h2>Password generator</h2>
+        
+        <div className='passwordboxin'>
+          <input type='text' value={fpass} readOnly /> <button onClick={copyPass}>copy</button>
+        </div>
+        <div className='passlenth'>
+             <lebel>Password length</lebel>
+             <input type='number' min={8} max={20} value={passlen}  onChange={(event)=>SetPasslen(event.target.value)} /> 
+        </div>
+
+        <div className='passlenth'>
+             <lebel>Include Uppercase letter </lebel>
+             <input type='checkbox' checked={uppercase} onChange={()=>setUppercase(!uppercase)}/> 
+        </div>
+
+         <div className='passlenth'>
+             <lebel>Include lowercase letter </lebel>
+             <input type='checkbox' checked={lowercase} onChange={()=>setLowercase(!lowercase)} /> 
+        </div>
+
+         <div className='passlenth'>
+             <lebel>Include numbers </lebel>
+             <input type='checkbox' checked={number} onChange={()=>setNumber(!number)} /> 
+        </div>
+
+         <div className='passlenth'>
+             <lebel>Include symbols </lebel>
+             <input type='checkbox' checked={symbol} onChange={()=>setSymbol(!symbol)} /> 
+        </div>
+        <div className='btn'>
+          <button onClick={createPasword}> Generate password</button>
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
